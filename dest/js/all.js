41f3dbd6d3e13ee83d59b21d6452a5473ae6edaa
brighttang2016 <798758642@qq.com>
2017-04-28 18:11:56 +0800

    添加minifyhtml插件、添加gulp clean

---
+++ dest/js/all.js
@@ -0,0 +1,101 @@
+/**
+ * Created by pujjr on 2017/3/7.
+ */
+$(document).ready(function(){
+   /* $("#login").bind("click",function(){
+       alert("ttttttt");
+        $.ajax({
+            url:'http://localhost:8080/tlmis/login/userLogin2',
+            type:'POST',
+            jsonp: "callback",
+            dataType:'json',
+            contentType:'text/plain;charset=UTF-8',
+            data:'{"name":"Ã∆¡¡123"}',
+            success:function(data){
+                alert(data);
+            }
+        });
+    });*/
+
+    /*$("#login").bind("click",function(){
+        alert("ttttttt");
+        $.ajax({
+            url:'http://localhost:8080/tlmis/login/userLogin2',
+            type:'POST',
+            dataType:'json',
+            contentType:'text/plain;charset=UTF-8',
+            data:'{"name":"Ã∆¡¡123"}',
+            success:function(data){
+                alert(data);
+            }
+        });
+    });*/
+    $("#login").bind("click",function(){
+         alert("ttttttt");
+         $.ajax({
+             url:'http://localhost:8080/tlms-web/login/userLogin',
+             type:'POST',
+             dataType:'json',
+             contentType:'text/plain;charset=UTF-8',
+             data:'{"name":"Ã∆¡¡123"}',
+             success:function(data, textStatus){
+                alert(data);
+             },
+             error:function(XMLHttpRequest, textStatus, errorThrown){
+                alert(XMLHttpRequest+"|"+textStatus+"|"+errorThrown);
+                 alert(XMLHttpRequest);
+             }
+         });
+     });
+});
+
+/**
+ * Created by pujjr on 2017/3/7.
+ */
+$(document).ready(function(){
+   /* $("#login").bind("click",function(){
+       alert("ttttttt");
+        $.ajax({
+            url:'http://localhost:8080/tlmis/login/userLogin2',
+            type:'POST',
+            jsonp: "callback",
+            dataType:'json',
+            contentType:'text/plain;charset=UTF-8',
+            data:'{"name":"Ã∆¡¡123"}',
+            success:function(data){
+                alert(data);
+            }
+        });
+    });*/
+
+    /*$("#login").bind("click",function(){
+        alert("ttttttt");
+        $.ajax({
+            url:'http://localhost:8080/tlmis/login/userLogin2',
+            type:'POST',
+            dataType:'json',
+            contentType:'text/plain;charset=UTF-8',
+            data:'{"name":"Ã∆¡¡123"}',
+            success:function(data){
+                alert(data);
+            }
+        });
+    });*/
+    $("#login").bind("click",function(){
+         alert("ttttttt");
+         $.ajax({
+             url:'http://localhost:8080/tlms-web/login/userLogin',
+             type:'POST',
+             dataType:'json',
+             contentType:'text/plain;charset=UTF-8',
+             data:'{"name":"Ã∆¡¡123"}',
+             success:function(data, textStatus){
+                alert(data);
+             },
+             error:function(XMLHttpRequest, textStatus, errorThrown){
+                alert(XMLHttpRequest+"|"+textStatus+"|"+errorThrown);
+                 alert(XMLHttpRequest);
+             }
+         });
+     });
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,68 @@
-var gulp = require('gulp'),
+var gulp = require('gulp')
     minifycss=require('gulp-minify-css'),
     concat=require('gulp-concat'),
     uglify=require('gulp-uglify'),
     rename=require('gulp-rename'),
-    jshint=require('gulp-jshint');
+    jshint=require('gulp-jshint'),
+    clean=require('gulp-clean'),
+    minifyhtml=require('gulp-minify-html');
 
-var src_css='./asset/css',
-    src_js='./asset/js',
-    dest_css='./dest/css',
-    dest_js='./dest/js';
-
-gulp.task('mincss',function(){
-   gulp.src(src_css+'/!**!/!*.css')
-       .pipe(mincss())
-       .pipe(gulp.dest(dest_css));
+var src_css='asset/!**',
+    dest_css='dest',
+    src_js = 'asset/js',
+    dest_js='dest';
+gulp.task('minifycss',function(){
+   gulp.src('asset/**/*.css')
+       .pipe(rename({suffix:'.min'}))
+       .pipe(minifycss())
+       .pipe(gulp.dest('dest'));
 });
 
-gulp.task('minjs',function(){
-    gulp.src(src_js+'/**/*.js')
+gulp.task('minifyjs',function(){
+    gulp.src('asset/**/*.js')
+        .pipe(rename({suffix:'.min'}))
         .pipe(uglify())
-        .pipe(gulp.dest(dest_js));
+        .pipe(gulp.dest('dest'));
+});
+
+gulp.task('minifyhtml',function(){
+    gulp.src('app/**/*.html')
+        .pipe(minifyhtml())
+        .pipe(gulp.dest('dest/html'))
+});
+
+gulp.task('concat',function(){
+    gulp.src('asset/**/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('dest/js'));
 });
 
 
-gulp.task("watch",function(){
+/*gulp.task("watch",function(){
     gulp.watch(src_css+'/!**!/!*.css',['mincss']);
     gulp.watch(src_js+'/!**!/!*.js',['minjs']);
-});
+});*/
 
 /*gulp.task("default",function(){
     gulp.run('minjs','mincss');
     gulp.run('watch');
 });*/
 
-gulp.task("watch",function(){
+/*gulp.task("watch",function(){
     gulp.watch(src_js+'/!**!/!*.js',['minjs']);
+});*/
+
+gulp.task('clean',function(){
+    setTimeout(function(){
+        gulp.src(['dest/**/*.css','dest/**/*.js','dest/**/*.html'])
+            .pipe(clean());
+    },10);
 });
 
-gulp.task("default",function(){
-   gulp.start('minjs','mincss');
-    gulp.start('watch');
+gulp.task('dobefore',function(){
+    console.log(" do before default");
+});
+
+gulp.task('default',['dobefore'],function(){
+   gulp.start('minifycss','minifyjs','minifyhtml','concat');
 });
